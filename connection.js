53a9308e12f1243f89ab822f3af534c3f4f15585
m-triple-m <triplem656@gmail.com>
2023-11-07 10:45:22 +0530

    added db url

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.DB_URL)
+  .connect('mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/expressapi?retryWrites=true&w=majority')
   .then((result) => {
     console.log("database connected");
   })
