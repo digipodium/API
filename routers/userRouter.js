d73dcee0555eb74147b98aae5974b158ad954cdb
m-triple-m <triplem656@gmail.com>
2023-11-10 20:14:16 +0530

    added user delete route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,55 +1,61 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const Model = require('../models/userModel');
+const Model = require("../models/userModel");
 
-router.post('/add', (req, res) => {
-    
-    new Model(req.body).save()
+router.post("/add", (req, res) => {
+  new Model(req.body)
+    .save()
     .then((result) => {
-        res.json(result);
+      res.json(result);
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
+      console.error(err);
+      res.status(500).json(err);
     });
 });
 
-router.post('/auth', (req, res) => {
-    
-    Model.findOne(req.body)
+router.post("/auth", (req, res) => {
+  Model.findOne(req.body)
     .then((result) => {
-        if(result) res.json(result);
-        else res.status(401).json({message : 'login failed'})
+      if (result) res.json(result);
+      else res.status(401).json({ message: "login failed" });
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
+      console.error(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/getall', (req, res) => {
-    
-    Model.find({})
+router.get("/getall", (req, res) => {
+  Model.find({})
     .then((result) => {
-        if(result) res.json(result);
-        else res.status(401).json({message : 'login failed'})
+      res.status(200).json(result);
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
+      console.error(err);
+      res.status(500).json(err);
     });
 });
 
-router.put('/update/:id', (req, res) => {
-    
-    Model.findByIdAndUpdate(req.params.id, req.body)
+router.put("/update/:id", (req, res) => {
+  Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then((result) => {
-        if(result) res.json(result);
-        else res.status(401).json({message : 'login failed'})
+      res.status(200).json(result);
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
     });
 });
 
